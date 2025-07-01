933f71e786cde0853f7a5dd52d2e27df65d49344
Charlie <charlie.mass.cm@gmail.com>
2025-07-01 23:53:47 +0200

    defined file structure in frontend + added oh vue icons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="min-h-screen">
-    <router-view />
-  </div>
+  <DefaultLayout />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import DefaultLayout from './layouts/DefaultLayout.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    DefaultLayout,
+  },
+});
+</script>
---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Menu from './Menu.vue';
+import Footer from './Footer.vue';
+
+export default defineComponent({
+  name: 'DefaultLayout',
+  components: {
+    Menu,
+    Footer,
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <Menu />
+    <main class="flex-grow">
+      <router-view />
+    </main>
+    <Footer />
+  </div>
+</template>
