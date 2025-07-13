880175ef762653d417cde7742e1eaab36a0f437c
CharlieMasson <charlie.mass.cm@gmail.com>
2025-07-13 16:09:14 +0200

    removed deprecated profile page and modal

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,14 +3,12 @@ import Home from '../pages/Home.vue';
 import Login from '../pages/Login.vue';
 import UIPreview from '../pages/UIPreview.vue';
 import Register from '@/pages/Register.vue';
-import Profile from '../pages/Profile.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
   { path: '/ui', name: 'UIPreview', component: UIPreview },
-  { path: '/profile', name: 'Profile', component: Profile },
 ];
 
 const router = createRouter({
